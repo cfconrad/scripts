210f28d825d0c0f91fca3a6d23c8e93f29344413
Clemens Famulla-Conrad <cfamullaconrad@suse.de>
2019-07-10 13:08:52 +0200

    Fix cleanup images
    Group images by flavor,version and delete all older images in each
    category.
    So we only keep one image per flavor.

--- publiccloud/azure/cleanup_images.pl
+++ publiccloud/azure/cleanup_images.pl
@@ -2,31 +2,68 @@ use Mojo::Base 'base';
 use Mojo::JSON qw(decode_json encode_json);
 use Date::Parse;
 use Mojo::File 'path';
+use Data::Dumper;
 
-my $out = `az storage blob list --account-name openqa --container-name sle-images`;
-#my $out = path('data.cache')->slurp();
-die("list blob failed") if ($?);
+my $rgroup = 'openqa-upload';
+my $storage_acc = 'openqa';
+my $storage_container = 'sle-images';
+my $keep_img = 1;
 
-my $data = decode_json($out);
-
-my @sorted_data = sort {str2time($a->{properties}->{creationTime}) cmp str2time($b->{properties}->{creationTime})} @{$data};
-for(my $i = 0; $i < @sorted_data - 3; $i++){
-    my $img = $sorted_data[$i];
-    print "DELETE : ";
-    print $img->{properties}->{creationTime};
-    print " ";
-    print $img->{name};
-    print $/;
-    system("az storage blob delete --account-name openqa --container-name sle-images --name " . $img->{name});
+sub run {
+    my ($cmd) = @_;
+    print (" RUN: " . $cmd . $/);
+    my $out = `$cmd`;
+    die("FAILED: $cmd") if ($?);
+    return $out;
 }
 
-for(my $i = @sorted_data - 3; $i < @sorted_data; $i++){
-    my $img = $sorted_data[$i];
-    print "KEEP: ";
-    print $img->{properties}->{creationTime};
-    print " ";
-    print $img->{name};
-    print $/;
+sub annotate_images
+{
+    my ($data) = @_;
+    my $img_by_group = {};
+    my $ret = {};
+    for my $i (@{$data}){
+        #            'name' => 'SLES12-SP5-Azure.x86_64-0.1.0-BYOS-Build1.14.vhd',
+        my $name = $i->{name};
+        my ($prefix, $build) = $name =~ /^(SLES.+)-Build(\d+\.\d+)\.vhd$/;
+        $img_by_group->{$prefix} //= [];
+        push(@{$img_by_group->{$prefix}}, { name => $name, build => $build,  creationTime => str2time($i->{properties}->{creationTime}), delete_me => 0});
+    }
+    for my $key (keys %{$img_by_group}){
+        my @sorted_data = sort {$a->{build} cmp $b->{build}} @{$img_by_group->{$key}};
+        for(my $i = 0; $i < @sorted_data; $i++){
+
+            # Mark all older images to be deleted
+            $sorted_data[$i]->{delete_me} = 1 if ($i < @sorted_data - $keep_img);
+            $sorted_data[$i]->{delete_me} = 1 if ($sorted_data[$i]->{creationTime} < time() - 3600 * 24 * 30);
+
+            # Store images in return hash
+            $ret->{$sorted_data[$i]->{name}} = $sorted_data[$i];
+        }
+        $img_by_group->{$key} = \@sorted_data;
+    }
+    return $ret;
 }
 
+my $data = decode_json(run("az storage blob list --account-name $storage_acc --container-name $storage_container"));
+my $images = annotate_images($data);
 
+while (my ($name, $img) = each %{$images}) {
+    next if ($img->{delete_me} != 1);
+    print ("DELETE : " . $name . $/);
+    run("az storage blob delete --account-name $storage_acc --container-name $storage_container --name " . $name);
+}
+while (my ($name, $img) = each %{$images}) {
+    next if ($img->{delete_me} != 0);
+    print ("KEEP : " . $name . $/);
+}
+
+$data = decode_json(run("az resource list --resource-group $rgroup"));
+for my $d (@{$data}){
+    next if (!exists($images->{$d->{name}}) || $images->{$d->{name}}->{delete_me} != 1);
+    if ($d->{'type'} eq 'Microsoft.Compute/images'){
+        run("az image delete --resource-group '$rgroup' --name '". $d->{name} . "'");
+    } elsif ($d->{type} eq 'Microsoft.Compute/disks' ) {
+        run("az disk delete --resource-group '$rgroup' --name '". $d->{name} . "' -y");
+    }
+}
